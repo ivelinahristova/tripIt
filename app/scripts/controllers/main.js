2864ff7a295fa0a66b6cbccb6591267b1c5d7e7b
Ivelina Hristova <iv4etoxx@gmail.com>
2015-06-14 23:53:28 +0300

    Include Trips in the Main page / Home

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,28 @@
  */
 angular.module('tripItApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.trips = [
+        {
+            id: 1,
+            startDate: 1,
+            endDate: 2,
+            locations: [
+                {
+                    name: 'Varna',
+                    date: 1,
+                    lat: 1,
+                    long: 2
+                }
+            ],
+            name: 'Trip To Varna'
+        },
+        {
+            id: 2,
+            name: 'Trip To Krushuna'
+        },
+        {
+            id: 3,
+            name: 'Trip To Kulata'
+        }
     ];
   });
