c60875fc16a82da685ee73c010f3a1cc4afb9372
Ivelina Hristova <iv4etoxx@gmail.com>
2015-06-24 02:28:45 +0300

    Home page add trips list

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,29 +8,19 @@
  * Controller of the tripItApp
  */
 angular.module('tripItApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.trips = [
-        {
-            id: 1,
-            startDate: 1,
-            endDate: 2,
-            locations: [
-                {
-                    name: 'Varna',
-                    date: 1,
-                    lat: 1,
-                    long: 2
-                }
-            ],
-            name: 'Trip To Varna'
-        },
-        {
-            id: 2,
-            name: 'Trip To Krushuna'
-        },
-        {
-            id: 3,
-            name: 'Trip To Kulata'
-        }
-    ];
+  .controller('MainCtrl', function ($scope, localStorageService) {
+        $scope.storageType = localStorageService.getStorageType();
+        var getTrips = function() {
+            var trips = [];
+            $.each(localStorageService.keys(), function(index, value){
+                trips.push(localStorageService.get(value));
+            });
+
+            return trips;
+        };
+        $scope.trips = getTrips();
+
+    if(localStorageService.isSupported) {
+
+    }
   });
